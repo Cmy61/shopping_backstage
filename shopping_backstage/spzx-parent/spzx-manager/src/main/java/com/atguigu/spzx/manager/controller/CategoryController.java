5d90f48ffa1395a4432c727a264c2c8abfcbf5c9
Muyang Chen <chenmuyangduoduo@126.com>
2025-01-10 15:21:11 -0800

    053 category preparation

---
+++ shopping_backstage/spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/CategoryController.java
@@ -0,0 +1,15 @@
+package com.atguigu.spzx.manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @descriptions:
+ * @author: cmy
+ * @date: 2025/1/10 15:09
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping(value="/admin/product/category")
+public class CategoryController {
+}
